58be1769b1a931e6704d4444049f2a88dd54b5eb
LI Geng <geng.li@qunar.com>
2019-02-15 15:16:52 +0800

    add version number string

--- jabber-qim-exts/jabber-qim-env.el
+++ jabber-qim-exts/jabber-qim-env.el
@@ -17,6 +17,8 @@
 ;; Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 
+(defvar *jabber-qim-version* "11001010")
+
 (defvar jabber-qim-local-file-dir
   "~/qim-local-files")
 
